b716b48c14d43af7ca70b9f6eff3820abb280df2
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2016-10-21 16:45:59 +1100

    initial commit.

--- server/instrumentation/__init__.py
+++ server/instrumentation/__init__.py
@@ -1,16 +1,21 @@
-import os
 import newrelic.agent
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def init_app(app):
     try:
-        newrelic.agent.initialize('/home/superdesk/temp/newrelic/newrelic.ini')
-        newrelic_module = newrelic.agent
         app.on_pre_GET += on_pre_request_get_callback
         app.on_post_GET += on_post_request_get_callback
-    except ImportError:
-        pass
-    except Exception as e:
-        print(e)
+        app.on_pre_POST += on_pre_request_post_callback
+        app.on_post_POST += on_post_request_post_callback
+        app.on_pre_PATCH += on_pre_request_patch_callback
+        app.on_post_PATCH += on_post_request_patch_callback
+        app.on_pre_DELETE += on_pre_request_delete_callback
+        app.on_post_DELETE += on_post_request_delete_callback
+    except Exception:
+        logger.error('Failed to start instrumentation.')
 
 
 def _is_profile_resource(resource):
@@ -18,22 +23,55 @@ def _is_profile_resource(resource):
 
 
 def on_pre_request_get_callback(resource, request, lookup):
-    if _is_profile_resource(resource):
-        print('on_request_start {} --- {} --- {}'.format(resource, request, lookup))
-        name = resource
-        group = 'Python/EveFramework/EndPoint'
-        try:
-            newrelic.agent.set_transaction_name(name, group=group)
-        except Exception as e:
-            print('failed to set transaction name. {}'.format(e))
+    newrelic_start_transaction(resource, request, lookup, 'Get')
 
 
 def on_post_request_get_callback(resource, request, lookup):
+    newrelic_end_transaction(resource, request, lookup, 'Get')
+
+
+def on_pre_request_post_callback(resource, request):
+    newrelic_start_transaction(resource, request, None, 'Post')
+
+
+def on_post_request_post_callback(resource, request, payload):
+    newrelic_end_transaction(resource, request, payload, 'Post')
+
+
+def on_pre_request_patch_callback(resource, request, payload):
+    newrelic_start_transaction(resource, request, payload, 'Patch')
+
+
+def on_post_request_patch_callback(resource, request, payload):
+    newrelic_end_transaction(resource, request, payload, 'Patch')
+
+
+def on_pre_request_delete_callback(resource, request, lookup):
+    newrelic_start_transaction(resource, request, lookup, 'Delete')
+
+
+def on_post_request_delete_callback(resource, request, lookup):
+    newrelic_end_transaction(resource, request, lookup, 'Delete')
+
+
+def newrelic_start_transaction(resource, request, lookup, method):
+    name = resource
+    group = 'Python/eve.endpoints/{}'.format(method)
+    if not resource:
+        return
     try:
+        newrelic.agent.set_transaction_name(name, group=group)
+        logger.info('on_request_start {} --- {} --- {}'.format(resource, request, lookup))
+    except Exception:
+        logger.error('failed to start transaction name. {} --- {}'.format(resource, method))
+
+
+def newrelic_end_transaction(resource, request, lookup, method):
+    try:
+        if not resource:
+            return
         if _is_profile_resource(resource):
-            print('on_request_start {} --- {} --- {}'.format(resource, request, lookup))
             newrelic.agent.end_of_transaction()
-    except Exception as e:
-        print('failed to set transaction name. {}'.format(e))
-
-
+            logger.info('on_request_start {} --- {} --- {}'.format(resource, request, lookup))
+    except Exception:
+        logger.error('failed to end transaction name. {} --- {}'.format(resource, method))
